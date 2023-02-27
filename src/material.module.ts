a3461b5b1de5f2413c2d144ecc8239f414d86290
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-02-27 15:13:34 +0600

    master

--- src/material.module.ts
+++ src/material.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from "@angular/material/card";
-import {MatRadioModule} from "@angular/material/radio";
-import {MatButtonModule} from "@angular/material/button";
-import {MatTableModule} from "@angular/material/table"
-import {MatPaginatorModule} from "@angular/material/paginator"
-import {MatSortModule} from "@angular/material/sort"
-import {MatDialogModule} from "@angular/material/dialog"
-import {MatSelectModule} from "@angular/material/select"
-import {MatCheckboxModule} from "@angular/material/checkbox"
+import { MatRadioModule } from "@angular/material/radio";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table"
+import { MatPaginatorModule } from "@angular/material/paginator"
+import { MatSortModule } from "@angular/material/sort"
+import { MatDialogModule } from "@angular/material/dialog"
+import { MatSelectModule } from "@angular/material/select"
+import { MatCheckboxModule } from "@angular/material/checkbox"
 
 
 @NgModule({
